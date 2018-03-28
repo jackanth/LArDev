e577038c4cd6f46f10b11ded12099cbcf10542ca
Jack Anthony <jackanth3@gmail.com>
2018-03-28 16:08:05 +0100

    Updated workspace and script.

--- shell_scripts/tidy_up.sh
+++ shell_scripts/tidy_up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -a searchPaths=("$PANDORA_LAR_PHYSICS_CONTENT_PATH/include" "$PANDORA_LAR_PHYSICS_CONTENT_PATH/src" "$PANDORA_LAR_PHYSICS_CONTENT_PATH/root")
+declare -a searchPaths=("$PANDORA_LAR_PHYSICS_CONTENT_PATH/larphysicscontent" "$PANDORA_LAR_PHYSICS_CONTENT_PATH/root")
 
 for i in "${searchPaths[@]}"
 do
